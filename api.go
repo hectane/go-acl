ed50c9222e614f0d1af2a740035eb9634ebb599c
Nathan Osman <nathan@quickmediasolutions.com>
2015-10-29 19:42:51 -0700

    Added SetEntriesInAcl() function.

--- api.go
+++ api.go
@@ -27,7 +27,7 @@ const (
 	SE_REGISTRY_WOW64_32KEY
 )
 
-// SECURITY_INFORMATION constants.
+// SECURITY_INFORMATION flags.
 const (
 	OWNER_SECURITY_INFORMATION               = 0x00001
 	GROUP_SECURITY_INFORMATION               = 0x00002
@@ -45,33 +45,98 @@ const (
 	UNPROTECTED_SACL_SECURITY_INFORMATION = 0x10000000
 )
 
+// MULTIPLE_TRUSTEE_OPERATION enumeration.
+const (
+	NO_MULTIPLE_TRUSTEE = iota
+	TRUSTEE_IS_IMPERSONATE
+)
+
+// TRUSTEE_FORM enumeration.
+const (
+	TRUSTEE_IS_SID = iota
+	TRUSTEE_IS_NAME
+	TRUSTEE_BAD_FORM
+	TRUSTEE_IS_OBJECTS_AND_SID
+	TRUSTEE_IS_OBJECTS_AND_NAME
+)
+
+// TRUSTEE_TYPE enumeration.
+const (
+	TRUSTEE_IS_UNKNOWN = iota
+	TRUSTEE_IS_USER
+	TRUSTEE_IS_GROUP
+	TRUSTEE_IS_DOMAIN
+	TRUSTEE_IS_ALIAS
+	TRUSTEE_IS_WELL_KNOWN_GROUP
+	TRUSTEE_IS_DELETED
+	TRUSTEE_IS_INVALID
+	TRUSTEE_IS_COMPUTER
+)
+
+// ACCESS_MODE enumeration.
+const (
+	NOT_USED_ACCESS = iota
+	GRANT_ACCESS
+	SET_ACCESS
+	DENY_ACCESS
+	REVOKE_ACCESS
+	SET_AUDIT_SUCCESS
+	SET_AUDIT_FAILURE
+)
+
+// ACE flags.
+const (
+	NO_INHERITANCE                     = 0x0
+	SUB_OBJECTS_ONLY_INHERIT           = 0x1
+	SUB_CONTAINERS_ONLY_INHERIT        = 0x2
+	SUB_CONTAINERS_AND_OBJECTS_INHERIT = 0x3
+	INHERIT_NO_PROPAGATE               = 0x4
+	INHERIT_ONLY                       = 0x8
+
+	OBJECT_INHERIT_ACE       = 0x1
+	CONTAINER_INHERIT_ACE    = 0x2
+	NO_PROPAGATE_INHERIT_ACE = 0x4
+	INHERIT_ONLY_ACE         = 0x8
+)
+
 var (
 	advapi32 = windows.MustLoadDLL("advapi32.dll")
 
 	procGetNamedSecurityInfoW = advapi32.MustFindProc("GetNamedSecurityInfoW")
 	procSetNamedSecurityInfoW = advapi32.MustFindProc("SetNamedSecurityInfoW")
+	procSetEntriesInAclW      = advapi32.MustFindProc("SetEntriesInAclW")
 )
 
-type ACL struct {
-	AclRevision byte
-	Sbz1        byte
-	AclSize     uint16
-	AceCount    uint16
-	Sbz2        uint16
+type ACL struct{}
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa379636.aspx
+type Trustee struct {
+	MultipleTrustee          *Trustee
+	MultipleTrusteeOperation int32
+	TrusteeForm              int32
+	TrusteeType              int32
+	Name                     *uint16
 }
 
-// Retrieve a copy of the security descriptor for the specified object. Note
-// that ppSecurityDescriptor must be freed using windows.LocalFree().
-func GetNamedSecurityInfo(pObjectName string, ObjectType int32, SecurityInfo uint32, ppsidOwner, ppsidGroup **windows.SID, ppDacl, ppSacl **ACL, ppSecurityDescriptor *windows.Handle) error {
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa446627.aspx
+type ExplicitAccess struct {
+	AccessPermissions uint32
+	AccessMode        int32
+	Inheritance       uint32
+	Trustee           Trustee
+}
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa446645.aspx
+func GetNamedSecurityInfo(objectName string, objectType int32, secInfo uint32, owner, group **windows.SID, dacl, sacl **ACL, secDesc *windows.Handle) error {
 	ret, _, err := procGetNamedSecurityInfoW.Call(
-		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr(pObjectName))),
-		uintptr(ObjectType),
-		uintptr(SecurityInfo),
-		uintptr(unsafe.Pointer(ppsidOwner)),
-		uintptr(unsafe.Pointer(ppsidGroup)),
-		uintptr(unsafe.Pointer(ppDacl)),
-		uintptr(unsafe.Pointer(ppSacl)),
-		uintptr(unsafe.Pointer(ppSecurityDescriptor)),
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr(objectName))),
+		uintptr(objectType),
+		uintptr(secInfo),
+		uintptr(unsafe.Pointer(owner)),
+		uintptr(unsafe.Pointer(group)),
+		uintptr(unsafe.Pointer(dacl)),
+		uintptr(unsafe.Pointer(sacl)),
+		uintptr(unsafe.Pointer(secDesc)),
 	)
 	if ret != ERROR_SUCCESS {
 		return err
@@ -79,16 +144,30 @@ func GetNamedSecurityInfo(pObjectName string, ObjectType int32, SecurityInfo uin
 	return nil
 }
 
-// Set the sepecified information in the object's security descriptor.
-func SetNamedSecurityInfo(pObjectName string, ObjectType int32, SecurityInfo uint32, psidOwner, psidGroup *windows.SID, pDacl, pSacl *ACL) error {
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa379579.aspx
+func SetNamedSecurityInfo(objectName string, objectType int32, secInfo uint32, owner, group *windows.SID, dacl, sacl *ACL) error {
 	ret, _, err := procSetNamedSecurityInfoW.Call(
-		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr(pObjectName))),
-		uintptr(ObjectType),
-		uintptr(SecurityInfo),
-		uintptr(unsafe.Pointer(psidOwner)),
-		uintptr(unsafe.Pointer(psidGroup)),
-		uintptr(unsafe.Pointer(pDacl)),
-		uintptr(unsafe.Pointer(pSacl)),
+		uintptr(unsafe.Pointer(windows.StringToUTF16Ptr(objectName))),
+		uintptr(objectType),
+		uintptr(secInfo),
+		uintptr(unsafe.Pointer(owner)),
+		uintptr(unsafe.Pointer(group)),
+		uintptr(unsafe.Pointer(dacl)),
+		uintptr(unsafe.Pointer(sacl)),
+	)
+	if ret != ERROR_SUCCESS {
+		return err
+	}
+	return nil
+}
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa379576.aspx
+func SetEntriesInAcl(entries []ExplicitAccess, oldAcl *ACL, newAcl **ACL) error {
+	ret, _, err := procSetEntriesInAclW.Call(
+		uintptr(len(entries)),
+		uintptr(unsafe.Pointer(&entries[0])),
+		uintptr(unsafe.Pointer(oldAcl)),
+		uintptr(unsafe.Pointer(newAcl)),
 	)
 	if ret != ERROR_SUCCESS {
 		return err
