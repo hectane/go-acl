07c51bbd7a56a22d372242d9063f42ab0d233032
Nathan Osman <nathan@quickmediasolutions.com>
2015-10-31 20:28:47 -0700

    Updated Chmod() test.

--- chmod_test.go
+++ chmod_test.go
@@ -7,23 +7,23 @@ import (
 )
 
 func TestChmod(t *testing.T) {
-	p, err := ioutil.TempDir(os.TempDir(), "")
+	f, err := ioutil.TempDir(os.TempFile(), "")
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer os.RemoveAll(p)
-	if err := Chmod(p, 0); err != nil {
+	defer os.Remove(f)
+	if err := Chmod(f.Name(), 0); err != nil {
 		t.Fatal(err)
 	}
-	d, err := os.Open(p)
+	d, err := os.Open(f.Name())
 	if err == nil {
 		d.Close()
 		t.Fatal("owner able to access directory")
 	}
-	if err := Chmod(p, 0400); err != nil {
+	if err := Chmod(f.Name(), 0400); err != nil {
 		t.Fatal(err)
 	}
-	d, err = os.Open(p)
+	d, err = os.Open(f.Name())
 	if err != nil {
 		t.Fatal("owner unable to access directory")
 	}
