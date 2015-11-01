f663bde7b16832b837794bfb5afe2745f61f9e6b
Nathan Osman <nathan@quickmediasolutions.com>
2015-10-31 20:27:29 -0700

    Simplified Chmod() implementation.

--- chmod.go
+++ chmod.go
@@ -10,40 +10,8 @@ import (
 
 // Change the permissions of the specified file. Only the nine
 // least-significant bytes are used, allowing access by the file's owner, the
-// file's group, and any other user to be explicitly controlled.
+// file's group, and everyone else to be explicitly controlled.
 func Chmod(name string, mode os.FileMode) error {
-	var (
-		sidOwner    = make([]byte, api.SECURITY_MAX_SID_SIZE)
-		sidOwnerLen = uint32(unsafe.Sizeof(sidOwner))
-		sidGroup    = make([]byte, api.SECURITY_MAX_SID_SIZE)
-		sidGroupLen = uint32(unsafe.Sizeof(sidGroup))
-		sidWorld    = make([]byte, api.SECURITY_MAX_SID_SIZE)
-		sidWorldLen = uint32(unsafe.Sizeof(sidWorld))
-	)
-	if err := api.CreateWellKnownSid(
-		api.WinCreatorOwnerSid,
-		nil,
-		(*windows.SID)(unsafe.Pointer(&sidOwner[0])),
-		&sidOwnerLen,
-	); err != nil {
-		return err
-	}
-	if err := api.CreateWellKnownSid(
-		api.WinCreatorGroupSid,
-		nil,
-		(*windows.SID)(unsafe.Pointer(&sidGroup[0])),
-		&sidGroupLen,
-	); err != nil {
-		return err
-	}
-	if err := api.CreateWellKnownSid(
-		api.WinWorldSid,
-		nil,
-		(*windows.SID)(unsafe.Pointer(&sidWorld[0])),
-		&sidWorldLen,
-	); err != nil {
-		return err
-	}
 	var (
 		entries = []api.ExplicitAccess{
 			{
@@ -51,8 +19,8 @@ func Chmod(name string, mode os.FileMode) error {
 				AccessMode:        api.GRANT_ACCESS,
 				Inheritance:       api.NO_INHERITANCE,
 				Trustee: api.Trustee{
-					TrusteeForm: api.TRUSTEE_IS_SID,
-					Name:        (*uint16)(unsafe.Pointer(&sidOwner[0])),
+					TrusteeForm: api.TRUSTEE_IS_NAME,
+					Name:        windows.StringToUTF16Ptr("CREATOR OWNER"),
 				},
 			},
 			{
@@ -60,8 +28,8 @@ func Chmod(name string, mode os.FileMode) error {
 				AccessMode:        api.GRANT_ACCESS,
 				Inheritance:       api.NO_INHERITANCE,
 				Trustee: api.Trustee{
-					TrusteeForm: api.TRUSTEE_IS_SID,
-					Name:        (*uint16)(unsafe.Pointer(&sidGroup[0])),
+					TrusteeForm: api.TRUSTEE_IS_NAME,
+					Name:        windows.StringToUTF16Ptr("CREATOR GROUP"),
 				},
 			},
 			{
@@ -69,14 +37,18 @@ func Chmod(name string, mode os.FileMode) error {
 				AccessMode:        api.GRANT_ACCESS,
 				Inheritance:       api.NO_INHERITANCE,
 				Trustee: api.Trustee{
-					TrusteeForm: api.TRUSTEE_IS_SID,
-					Name:        (*uint16)(unsafe.Pointer(&sidWorld[0])),
+					TrusteeForm: api.TRUSTEE_IS_NAME,
+					Name:        windows.StringToUTF16Ptr("EVERYONE"),
 				},
 			},
 		}
 		acl windows.Handle
 	)
-	if err := api.SetEntriesInAcl(entries, 0, &acl); err != nil {
+	if err := api.SetEntriesInAcl(
+		entries,
+		0,
+		&acl,
+	); err != nil {
 		return err
 	}
 	defer windows.LocalFree((windows.Handle)(unsafe.Pointer(acl)))
