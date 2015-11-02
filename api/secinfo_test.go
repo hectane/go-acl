23f1c5b54c6beaeabd6b044b56e02a2fee68a9a3
Nathan Osman <nathan@quickmediasolutions.com>
2015-11-02 13:58:03 -0800

    Fixed secinfo tests.

--- api/secinfo_test.go
+++ api/secinfo_test.go
@@ -15,14 +15,13 @@ func TestGetNamedSecurityInfo(t *testing.T) {
 	}
 	defer os.Remove(f.Name())
 	var (
-		ownerSid *windows.SID
-		secDesc  windows.Handle
+		secDesc windows.Handle
 	)
 	if err = GetNamedSecurityInfo(
 		f.Name(),
 		SE_FILE_OBJECT,
-		OWNER_SECURITY_INFORMATION,
-		&ownerSid,
+		0,
+		nil,
 		nil,
 		nil,
 		nil,
@@ -31,22 +30,6 @@ func TestGetNamedSecurityInfo(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer windows.LocalFree(secDesc)
-	token, err := windows.OpenCurrentProcessToken()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer token.Close()
-	u, err := token.GetTokenUser()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if !windows.EqualSid(ownerSid, u.User.Sid) {
-		t.Log(ownerSid)
-		t.Log(ownerSid.String())
-		t.Log(u.User.Sid)
-		t.Log(u.User.Sid.String())
-		t.Fatal("SID of file does not match SID of current process")
-	}
 }
 
 func TestSetNamedSecurityInfo(t *testing.T) {
@@ -55,20 +38,11 @@ func TestSetNamedSecurityInfo(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer os.Remove(f.Name())
-	token, err := windows.OpenCurrentProcessToken()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer token.Close()
-	u, err := token.GetTokenUser()
-	if err != nil {
-		t.Fatal(err)
-	}
 	if err = SetNamedSecurityInfo(
 		f.Name(),
 		SE_FILE_OBJECT,
-		OWNER_SECURITY_INFORMATION,
-		u.User.Sid,
+		DACL_SECURITY_INFORMATION,
+		nil,
 		nil,
 		0,
 		0,
