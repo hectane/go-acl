6937c4c474eb92a9345bd6b02c5906a216102502
Nathan Osman <nathan@quickmediasolutions.com>
2019-01-12 12:57:48 -0800

    Use well-known SIDs for chmod (fixes #6).

--- chmod.go
+++ chmod.go
@@ -12,8 +12,8 @@ func Chmod(name string, mode os.FileMode) error {
 		name,
 		true,
 		false,
-		GrantName((uint32(mode)&0700)<<23, "CREATOR OWNER"),
-		GrantName((uint32(mode)&0070)<<26, "CREATOR GROUP"),
-		GrantName((uint32(mode)&0007)<<29, "EVERYONE"),
+		GrantName((uint32(mode)&0700)<<23, "S-1-3-0"),
+		GrantName((uint32(mode)&0070)<<26, "S-1-3-1"),
+		GrantName((uint32(mode)&0007)<<29, "S-1-1-0"),
 	)
 }
